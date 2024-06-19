d7301b0e37260ade284052d38661aae82694f937
Madihlaba, Kholofelo K <Kholofelo.Madihlaba@standardbank.co.za>
2024-06-19 14:23:57 +0200

    validate the file against bucket structure

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import boto3
 import urllib.parse
 import os
+from datetime import date
 
 ssm = boto3.client('ssm')
 s3 = boto3.client('s3')
@@ -17,6 +18,47 @@ branch, owner, repo, workflow_name, ghp_token = (
     ssm.get_parameter(Name=os.environ['ghp_token'])['Parameter']['Value'],
 
 )
+
+def validateFile(bucket, filename):
+
+    print('About to validate file=', filename)
+
+    today=date.today()
+    print("Today's date=", today)
+
+    filename_list=filename.split("/")
+    print('filename_list=', filename_list)
+
+    filename_final=filename_list[-1]
+    print('filename_final=', filename_final)
+
+    app_live='Deployment-Backups/Release/app-live/Vizru-App-live-Release-' + str(today)
+    print('app_live= {} and filename= {}'.format(app_live, filename))
+
+    chat_system='Deployment-Backups/Release/chatsystem/Vizru-chatSystem-Release-' + str(today)
+    print('chat_system= {} and filename= {}'.format(chat_system, filename))
+
+    docker='Deployment-Backups/Release/docker/Vizru-Docker-Release-' + str(today)
+    print('docker= {} and filename= {}'.format(docker, filename))
+
+    pdfgen='Deployment-Backups/Release/pdfgen/Vizru-pdfgen-Release-' + str(today)
+    print('pdfgen= {} and filename= {}'.format(pdfgen, filename))
+
+    pdfgenv2='Deployment-Backups/Release/pdfgenv2/Vizru-pdfgenv2-Release-' + str(today)
+    print('pdfgenv2= {} and filename= {}'.format(pdfgenv2, filename))
+
+    if filename.startswith(app_live.strip()):
+        return 's3://' + bucket + '/' + filename
+    elif filename.startswith(chat_system.strip()):
+        return 's3://' + bucket + '/' + filename
+    elif filename.startswith(docker.strip()):
+        return 's3://' + bucket + '/' + filename
+    elif filename.startswith(pdfgen.strip()):
+        return 's3://' + bucket + '/' + filename
+    elif filename.startswith(pdfgenv2.strip()):
+        return 's3://' + bucket + '/' + filename
+    else:
+        raise Exception("File {} is invalid in {} bucket, please verify the date or name of your file...".format(filename_final, filename_list[2]))
 
 def lambda_handler(event, context):
 
@@ -39,8 +81,8 @@ def lambda_handler(event, context):
             'Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(
                 file, bucket))
 
-    s3_uri = 's3://' + bucket + '/' + file
-    print('Bucket=' + bucket + ' File=' + file + ' s3_uri=' + s3_uri)
+    s3_uri=validateFile(bucket, file)
+    print('s3_uri=', s3_uri)
 
     # Github workflow
     deploy_headers = {
@@ -57,4 +99,7 @@ def lambda_handler(event, context):
     }
 
     response = requests.post(API_URL, headers=deploy_headers, data=json.dumps(deploy_body_data))
-    print('Status_Code {}, Response {}'.format(response.status_code, response.content))
+    if(response.status_code == 204):
+        print('The GitHub Workflow was triggered successfully...')
+    else:
+        print('The GitHub Workflow failed to be triggered with error code {} [Response: {}]'.format(response.status_code, response.content))
